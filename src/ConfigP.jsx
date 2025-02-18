368b7d048847721b9a54e0f49e059789933727be
Miracle <122048881+Miracles01@users.noreply.github.com>
2025-02-18 18:39:13 +0700

    okkk

--- src/ConfigP.jsx
+++ src/ConfigP.jsx
@@ -1,32 +1,13 @@
+import { BrowserRouter as Router } from "react-router-dom";
+import "./App.css";
 import { Config } from "./Components";
-import { useEffect, useState } from "react";
 
-const ConfigP = () => {
-  // สร้าง State เก็บขนาดของหน้าต่าง
-  const [windowSize, setWindowSize] = useState({
-    width: window.innerWidth,
-    height: window.innerHeight,
-  });
-
-  useEffect(() => {
-    // ฟังก์ชันอัพเดทขนาดเมื่อหน้าต่างเปลี่ยนแปลง
-    const handleResize = () => {
-      setWindowSize({
-        width: window.innerWidth,
-        height: window.innerHeight,
-      });
-    };
-
-    // เพิ่ม Event Listener
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize); // ลบ Event Listener เมื่อคอมโพเนนต์ unmount
-  }, []);
-
+function App() {
   return (
-    <div style={{ width: "100vw", height: "100vh", display: "flex", justifyContent: "center", alignItems: "center" }}>
-      <Config windowSize={windowSize} />
-    </div>
+    <Router>
+      <Config />
+    </Router>
   );
-};
+}
 
-export default ConfigP;
+export default App;
