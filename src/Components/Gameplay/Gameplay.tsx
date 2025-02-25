f70cadffdbc7b602655e3ed43f7e30c2228ff160
GUSTGUSTS <141816627+GUSTGUSTS@users.noreply.github.com>
2025-02-26 01:30:27 +0700

    64 HEXS

--- src/Components/Gameplay/Gameplay.tsx
+++ src/Components/Gameplay/Gameplay.tsx
@@ -4,8 +4,87 @@ import styles from './Gameplay.module.css';
 
 const Gameplay = () => {
   return (
-      <img className={styles.GameplayBG} alt="Gameplay Background" src="src\public\Gameplay\Game play b.png" />
-  );
+      <div>
+        <h1>"hello"</h1>
+        <div>
+          <img className={styles.GameplayBG} alt="Gameplay Background" src="src\public\Gameplay\Game play b.png"/>
+        </div>
+
+        <img className={styles.hex1} alt="" src="src\public\Gameplay\Hex\component.svg"/>
+        <img className={styles.hex2} alt="" src="src\public\Gameplay\Hex\component.svg"/>
+        <img className={styles.hex3} alt="" src="src\public\Gameplay\Hex\component.svg"/>
+        <img className={styles.hex4} alt="" src="src\public\Gameplay\Hex\component.svg"/>
+        <img className={styles.hex5} alt="" src="src\public\Gameplay\Hex\component.svg"/>
+        <img className={styles.hex6} alt="" src="src\public\Gameplay\Hex\component.svg"/>
+        <img className={styles.hex7} alt="" src="src\public\Gameplay\Hex\component.svg"/>
+        <img className={styles.hex8} alt="" src="src\public\Gameplay\Hex\component.svg"/>
+
+        <img className={styles.hex9} alt="" src="src\public\Gameplay\Hex\component.svg"/>
+        <img className={styles.hex10} alt="" src="src\public\Gameplay\Hex\component.svg"/>
+        <img className={styles.hex11} alt="" src="src\public\Gameplay\Hex\component.svg"/>
+        <img className={styles.hex12} alt="" src="src\public\Gameplay\Hex\component.svg"/>
+        <img className={styles.hex13} alt="" src="src\public\Gameplay\Hex\component.svg"/>
+        <img className={styles.hex14} alt="" src="src\public\Gameplay\Hex\component.svg"/>
+        <img className={styles.hex15} alt="" src="src\public\Gameplay\Hex\component.svg"/>
+        <img className={styles.hex16} alt="" src="src\public\Gameplay\Hex\component.svg"/>
+
+        <img className={styles.hex17} alt="" src="src\public\Gameplay\Hex\component.svg"/>
+        <img className={styles.hex18} alt="" src="src\public\Gameplay\Hex\component.svg"/>
+        <img className={styles.hex19} alt="" src="src\public\Gameplay\Hex\component.svg"/>
+        <img className={styles.hex20} alt="" src="src\public\Gameplay\Hex\component.svg"/>
+        <img className={styles.hex21} alt="" src="src\public\Gameplay\Hex\component.svg"/>
+        <img className={styles.hex22} alt="" src="src\public\Gameplay\Hex\component.svg"/>
+        <img className={styles.hex23} alt="" src="src\public\Gameplay\Hex\component.svg"/>
+        <img className={styles.hex24} alt="" src="src\public\Gameplay\Hex\component.svg"/>
+
+        <img className={styles.hex25} alt="" src="src\public\Gameplay\Hex\component.svg"/>
+        <img className={styles.hex26} alt="" src="src\public\Gameplay\Hex\component.svg"/>
+        <img className={styles.hex27} alt="" src="src\public\Gameplay\Hex\component.svg"/>
+        <img className={styles.hex28} alt="" src="src\public\Gameplay\Hex\component.svg"/>
+        <img className={styles.hex29} alt="" src="src\public\Gameplay\Hex\component.svg"/>
+        <img className={styles.hex30} alt="" src="src\public\Gameplay\Hex\component.svg"/>
+        <img className={styles.hex31} alt="" src="src\public\Gameplay\Hex\component.svg"/>
+        <img className={styles.hex32} alt="" src="src\public\Gameplay\Hex\component.svg"/>
+
+        <img className={styles.hex33} alt="" src="src\public\Gameplay\Hex\component.svg"/>
+        <img className={styles.hex34} alt="" src="src\public\Gameplay\Hex\component.svg"/>
+        <img className={styles.hex35} alt="" src="src\public\Gameplay\Hex\component.svg"/>
+        <img className={styles.hex36} alt="" src="src\public\Gameplay\Hex\component.svg"/>
+        <img className={styles.hex37} alt="" src="src\public\Gameplay\Hex\component.svg"/>
+        <img className={styles.hex38} alt="" src="src\public\Gameplay\Hex\component.svg"/>
+        <img className={styles.hex39} alt="" src="src\public\Gameplay\Hex\component.svg"/>
+        <img className={styles.hex40} alt="" src="src\public\Gameplay\Hex\component.svg"/>
+
+        <img className={styles.hex41} alt="" src="src\public\Gameplay\Hex\component.svg"/>
+        <img className={styles.hex42} alt="" src="src\public\Gameplay\Hex\component.svg"/>
+        <img className={styles.hex43} alt="" src="src\public\Gameplay\Hex\component.svg"/>
+        <img className={styles.hex44} alt="" src="src\public\Gameplay\Hex\component.svg"/>
+        <img className={styles.hex45} alt="" src="src\public\Gameplay\Hex\component.svg"/>
+        <img className={styles.hex46} alt="" src="src\public\Gameplay\Hex\component.svg"/>
+        <img className={styles.hex47} alt="" src="src\public\Gameplay\Hex\component.svg"/>
+        <img className={styles.hex48} alt="" src="src\public\Gameplay\Hex\component.svg"/>
+
+        <img className={styles.hex49} alt="" src="src\public\Gameplay\Hex\component.svg"/>
+        <img className={styles.hex50} alt="" src="src\public\Gameplay\Hex\component.svg"/>
+        <img className={styles.hex51} alt="" src="src\public\Gameplay\Hex\component.svg"/>
+        <img className={styles.hex52} alt="" src="src\public\Gameplay\Hex\component.svg"/>
+        <img className={styles.hex53} alt="" src="src\public\Gameplay\Hex\component.svg"/>
+        <img className={styles.hex54} alt="" src="src\public\Gameplay\Hex\component.svg"/>
+        <img className={styles.hex55} alt="" src="src\public\Gameplay\Hex\component.svg"/>
+        <img className={styles.hex56} alt="" src="src\public\Gameplay\Hex\component.svg"/>
+
+        <img className={styles.hex57} alt="" src="src\public\Gameplay\Hex\component.svg"/>
+        <img className={styles.hex58} alt="" src="src\public\Gameplay\Hex\component.svg"/>
+        <img className={styles.hex59} alt="" src="src\public\Gameplay\Hex\component.svg"/>
+        <img className={styles.hex60} alt="" src="src\public\Gameplay\Hex\component.svg"/>
+        <img className={styles.hex61} alt="" src="src\public\Gameplay\Hex\component.svg"/>
+        <img className={styles.hex62} alt="" src="src\public\Gameplay\Hex\component.svg"/>
+        <img className={styles.hex63} alt="" src="src\public\Gameplay\Hex\component.svg"/>
+        <img className={styles.hex64} alt="" src="src\public\Gameplay\Hex\component.svg"/>
+      </div>
+
+  )
+      ;
 };
 
 export default Gameplay;
