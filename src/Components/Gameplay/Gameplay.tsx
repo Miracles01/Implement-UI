abce409e9792142abfa5981a7492d23b8d15a1ee
Miracle <122048881+Miracles01@users.noreply.github.com>
2025-02-25 22:28:04 +0700

    dbug

---
+++ src/Components/Gameplay/Gameplay.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from './Gameplay.module.css';
+
+
+const Gameplay = () => {
+  return (
+      <img className={styles.GameplayBG} alt="Gameplay Background" src="src\public\Gameplay\Game play b.png" />
+  );
+};
+
+export default Gameplay;
